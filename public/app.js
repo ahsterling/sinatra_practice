35b64dc475fa60e7aac5af93c1a6ea0cc3946483
Allie Sterling <sterlina89@gmail.com>
2014-10-02 12:23:12 -0700

    fixed margin issues on header

--- public/app.js
+++
@@ -1,7 +0,0 @@
-$("#github").hover(
-  function() {
-    $(this).html("src","/github-6-64.png");
-  }, function () {
-    $(this).html("src", "/twitter-64");
-  }
-);
---
+++ public/javascripts/app.js
@@ -0,0 +1,27 @@
+
+
+// $("#github").hover(
+//   function() {
+//     $(this).html("background-image", "/twitter-64-lighter.png");
+//   },
+//   function() {
+//     $(this).css("background-image", "/twitter-64.png");
+//   }
+// );
+
+ // $("#github").hover(
+ //    function () {
+ //       $("#twitter").html("<img src = '/github-6-64.png'/>");
+ // }, function () {
+ //      $("#twitter").html("<img src = '/github-6-64.lighter.png'/>");
+ // }
+ // );
+
+// $(document).ready(function() {
+//   $(".page-heading").click(function() {
+//     $(this).css("color", "blue");
+//     var fontSize = $(this).css("font-size");
+//     console.log(fontSize);
+//     $(this).css("font-size", (Math.random() * 100) + "px");
+//   });
+// });
